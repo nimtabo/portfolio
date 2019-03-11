84cf6b0f0aa9f220876735b8b2b93957b399a47f
nimtabo <35834849+nimtabo@users.noreply.github.com>
2019-03-11 16:21:09 +0300

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
-  <div>
+  <div className="container">
     404
     Page not found. 
     <p><Link to="/">Go Back Home</Link></p>
